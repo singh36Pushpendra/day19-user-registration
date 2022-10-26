4709f00fcba383ae619d5865cffc5421215c73df
singh36Pushpendra <psthakur.1999@gmail.com>
2022-10-27 02:06:32 +0530

    Added: UC3 Email validation

--- src/com/user/register/UserRegistration.java
+++ src/com/user/register/UserRegistration.java
@@ -21,8 +21,14 @@ public class UserRegistration {
 
         final String LAST_NAME_PATTERN = FIRST_NAME_PATTERN;
 
+        System.out.println("Enter Email Id: ");
+        String email = scanner.next();
+
+        final String EMAIL_PATTERN = "^[a-zA-Z]+([.][a-zA-Z0-9]{1,})*[@][a-zA-Z]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{1,})*$";
+
         System.out.println("Is First Name Valid?: " + validate(FIRST_NAME_PATTERN, firstName));
         System.out.println("Is Last Name Valid?: " + validate(LAST_NAME_PATTERN, lastName));
+        System.out.println("Is Email Valid?: " + validate(EMAIL_PATTERN, email));
 
     }
 }
